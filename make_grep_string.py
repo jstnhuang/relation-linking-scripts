b69da0340836b8a9d1a1e9d1b8c69d8e93ddb0df
Justin Huang <jstn@cs.washington.edu>
2013-05-08 19:21:28 -0700

    Escape special characters.

--- make_grep_string.py
+++ make_grep_string.py
@@ -5,8 +5,12 @@ import sys
 
 def main():
   tuples = set()
+  specialChars = ['^', '$', '\\', '[', ']', '.', '*']
   for line in sys.stdin:
-    tuples.add(line.strip())
+    pattern = line.strip()
+    for specialChar in specialChars:
+      pattern = pattern.replace(specialChar, '\\' + specialChar)
+    tuples.add(pattern)
   print('grep \'{}\' $1'.format('\\|'.join(tuples)))
 
 if __name__ == '__main__':
